62575741670566231808aa71fd5368769cba61a0
santhoshpandi543 <skannan@eagleconsulting.co.in>
2026-07-16 17:00:59 +0530

    feat: implement centralized exception handling

--- app/common/errors.py
+++ app/common/errors.py
@@ -1,2 +1,25 @@
+from fastapi.responses import JSONResponse
+from fastapi import Request, HTTPException
 
+async def centralized_exception_handler(request: Request, exc: HTTPException):
 
+    try:
+        body_data = await request.json()
+    except Exception:
+        body_data = {}
+
+    error_response = {
+        "status_code": exc.status_code,
+        "message": exc.detail,
+        "metadata": {
+            "method": request.method,
+            "path": request.url.path,
+            "content": {
+                "body": body_data,
+                "query": dict(request.query_params),
+                "params": request.path_params,
+            },
+        },
+    }
+
+    return JSONResponse(status_code=exc.status_code, content=error_response)
--- app/main.py
+++ app/main.py
@@ -1,26 +1,30 @@
-from fastapi import FastAPI, Query, Depends, HTTPException
-from sqlalchemy.orm import Session
-from typing import Annotated
+from fastapi import FastAPI, HTTPException
 
 # Our File
 from app.routers import todos
-from app.schemas.todos import TodoResponse, TodoCreate, QueryParams
+from app.common.errors import centralized_exception_handler
 from app.database import engine, Base
-from app.models.todos import Todo as TodoModel
 
 # To tell Sqlalchemy to build the tables in Postgre
 Base.metadata.create_all(bind=engine)
 
 
-def create_app()->FastAPI:   
+def create_app() -> FastAPI:
 
     # app = FastAPI(dependencies=[Depends(get_db)])
-    app = FastAPI(title='Todo App')
+    app = FastAPI(
+        title="Todo App",
+        # To link the centralized Http exception
+        exception_handlers={HTTPException: centralized_exception_handler}
+    )
+
+    # To link the centralized Http exception this one another way 👇
+    # app.add_exception_handler(HTTPException, handler=centralized_exception_handler)
 
     # To link the various routers [users, products, orders]
     app.include_router(todos.router)
 
     return app
 
-app = create_app()
 
+app = create_app()
